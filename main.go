dea65c656061c0cace804a42759d66283a394f2a
pokuwagata <pokuwagata@gmail.com>
2020-02-08 19:54:42 +0900

    add serifs

--- main.go
+++ main.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
 
+var serifs = map[string][]string{
+	"nobue": []string{
+		"えーはじめまして。あたしは伊藤伸恵。ご覧のとおりごく普通の16歳です",
+		"長い付き合いだけど、こいつの頭ん中はよく分かりません",
+		"んじゃ、失礼",
+	},
+	"chika": []string{
+		"いい加減、玄関から入りなよみっちゃん",
+		"禁煙しろ",
+		"おまえ、アホだろ",
+	},
+	"miu": []string{
+		"君らここでなにしてんの？",
+		"灰皿を作りませんか？",
+		"よし、それでは今回作る灰皿の見本だが・・・",
+	},
+}
+
 func main() {
+	cli.AppHelpTemplate = `Name:
+	ichigo-cli - display ichigo-mashimaro serifs
+
+Usage:
+	ichigo-cli [options] [name]
+
+Example:
+	ichigo-cli nobue
+
+Options:
+	-h --help
+	-v --version
+
+Names:
+	nobue: 伸恵
+	chika: 千佳
+	miu: 美羽`
+
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
+		Name:  "ichigo-cli",
+		Usage: "display ichigo-mashimaro serifs",
 		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!")
-			return nil
+			return run(c)
 		},
 	}
 
@@ -22,4 +60,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run(c *cli.Context) error {
+	name := c.Args().Get(0)
+	if name == "" {
+		return errors.New("name is required")
+	}
+	if serifs[name] == nil {
+		return fmt.Errorf("%s is invalid", name)
+	}
+	size := len(serifs[name])
+
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(serifs[name][rand.Intn(size)])
+	return nil
 }
